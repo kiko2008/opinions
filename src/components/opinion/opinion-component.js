e8311bef2bac02b39c7d41cc277ec61acccc6f43
Francisco Fernandez <ffernandezpacheco@gmail.com>
2018-09-19 21:18:06 +0200

    Cambios en lista opiniones

--- src/components/opinion/opinion-component.js
+++ src/components/opinion/opinion-component.js
@@ -1,19 +1,25 @@
 export const createOpinion = ({
-  title, author, imageUrl, id
+  title, author, photoAuthor, mediaUrl, description, publicDate, numComments, id
 } = { title: 'No title', author: 'No author' }) => {
   const opinion = document.createElement('div');
   opinion.classList.add('opinion');
-  opinion.innerHTML =`<div class="opinion-image-container">
-                        <a class="opinion-title" href="/opinion/?id=${id}">
-                          <img src="${imageUrl}" class="opinion-image" ></img>
-                        </a>
-                      </div>
-                      <div class="opinion-title-container">
-                        <div>
-                          <a class="opinion-title" href="/opinion/?id=${id}">${title}</a>
+  opinion.innerHTML =`
+                        <div class="opinion-image-container">
+                          <a class="opinion-title" href="/opinion/?id=${id}">
+                            <img src="${mediaUrl}" class="opinion-image" ></img>
+                          </a>
                         </div>
-                        <p class="opinion-author">${author}</p>
-                      </div>`;
+                        <div class="opinion-title-container">
+                          <div>
+                            <a class="opinion-title" href="/opinion/?id=${id}">${title}</a>
+                          </div>
+                          <p class="opinion-description">${description}</p>
+                          <p class="opinion-date">${publicDate}</p>
+                          <p class="opinion-comments">${numComments} comentarios</p>
+                          <div class="opinion-author">${author}</div>  
+                        </div>
+                                               
+                      `;
   return opinion;
 };
 
